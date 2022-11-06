b666003539010ccf64386b9db270afd1b58bc5a4
adhamniyozboyev <adhamjonniyozboyev@gmail.com>
2022-11-06 15:45:29 +0500

    bin/basic_class_homework01.dart

--- bin/basic_class_homework01.dart
+++ bin/basic_class_homework01.dart
@@ -3,30 +3,10 @@ Create a "Person" class
 */
 
 
-
 class Person{
-  String gender='Female';
-  String name='Dilshoda';
-  int age =16;
-  int high=165;
-  Person ({required String gender,required String name,required int age,required int high}){
-    this.age=age;
-    this.name=name;
-    this.gender=gender;
-    this.high=high;
 
-  }
-
-    String mind(int calc){
-      return 'count the money\'s $calc dollar';
-    }
 
 }
-void main(){
-  Person ps=Person(age: 14,high: 172,gender: 'male',name: 'Adham');
-  print(ps.age);
-  print(ps.gender);
-  print(ps.high-7);
-  print(ps.name+' Niyozboyev');
-  print(ps.mind(123));
+void mann(){
+  print(Person());
 }
