84448d923374a6b7ce2c140bbb13ba17b780d9e1
adhamniyozboyev <adhamjonniyozboyev@gmail.com>
2022-11-06 19:32:00 +0500

    bin/basic_class_homework06.dart

--- bin/basic_class_homework06.dart
+++ bin/basic_class_homework06.dart
@@ -5,10 +5,10 @@ Create a "Person" class that has a name("name") and a age("age")
 */
 
 class Person{
-  String name='Adham';
+  String name='ali';
   int age=16;
 }
 void main() {
   print(Person());
-  
+
 }
