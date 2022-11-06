14fa657f131a18b4ec005e87089f125bcb3b59a6
adhamniyozboyev <adhamjonniyozboyev@gmail.com>
2022-11-06 17:35:17 +0500

    bin/basic_class_homework04.dart

--- bin/basic_class_homework04.dart
+++ bin/basic_class_homework04.dart
@@ -7,12 +7,14 @@ create an object named "p1" whose name is "Anvar"
 create an object named "p2" whose name is "Shavkat"
 */
 class Person{
-  String name='Adham';
+ String name = 'Anvar';
 
-  Person({required String p1,required String p2}){
+Person(String name ){
+  this.name = name;
+}}
 
-  }
-}
 void main() {
-  Person person=Person(p1: 'Anvar', p2: 'Shavkat');
-}
+  Person p1 = Person('Anvar');
+  Person p2 = Person('Shavkat');  
+  print(p1.name);
+  }
